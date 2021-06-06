d8f5b876a9162c5b73df048a35a2a9be22f91543
JustScratchCoder <85436576+JustScratchCoder@users.noreply.github.com>
2021-06-06 12:23:25 +0200

    Update program.bat

--- program.bat
+++ program.bat
@@ -18,6 +18,7 @@ echo Input: %inputf%
 echo Output: %outputf%
 echo Command: ffmpeg -i %inputf% -f mp4 -vf "fps=24000/1001, colorspace=space=ycgco:primaries=bt709:trc=bt709:range=pc:iprimaries=bt709:iall=bt709, scale=256:144" -dst_range 1 -color_range 2 -vcodec mpeg4 -profile:v 0 -level 8 -q:v 2 -maxrate 500k -acodec aac -ar 32k -b:a 64000 -ac 1 -slices 1 -g 50 %outputf%
 echo Press Space to convert
+pause >nul
 goto convert
 
 :convert:
